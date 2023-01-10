932aeb38826168b9dd217382cc5db24f1c6239b4
ananthana <anantha.nagaraj@ellucian.com>
2023-01-10 12:45:34 +0530

    Additional lint rules added

--- index.js
+++ index.js
@@ -8,30 +8,32 @@ module.export = {
     sourceType: "module",
   },
   rules: {
-    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-var-requires": "warn",
     "no-console": "warn",
     "comma-spacing": ["error"],
     "no-trailing-spaces": ["error"],
-    curly: ["error"],
+    "curly": ["error"],
     "eol-last": ["error"],
-    eqeqeq: "error",
-    indent: [
+	"prefer-const": "warn",
+	"no-empty-pattern": "warn",
+    "eqeqeq": "error",
+    "indent": [
       "warn",
       4,
       {
-        SwitchCase: 1,
+        "SwitchCase": 1,
       },
     ],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
     "space-before-blocks": [
       "error",
-      { functions: "always", keywords: "never", classes: "never" },
+      { "functions": "always", "keywords": "never", "classes": "never" },
     ],
     "no-empty": [
       "error",
       {
-        allowEmptyCatch: true,
+        "allowEmptyCatch": true,
       },
     ],
   },
